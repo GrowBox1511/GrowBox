327cb0e17bce46e053229a8dc005cf539cd511e1
tubond <57327504+20-TubonD-20@users.noreply.github.com>
2020-06-06 16:04:51 +0300

    Add files via upload
    testI2C connection Master Esp <-> Slave Uno  + FireBase + led

---
+++ I2Cnmcu.ino
@@ -0,0 +1,69 @@
+#include <Wire.h>
+#include <ESP8266HTTPClient.h>
+#include "FirebaseESP8266.h"
+
+#define FIREBASE_HOST "growbox-52071.firebaseio.com"
+
+#define FIREBASE_AUTH "ODA5KgiodLXzwCcoekNBOhfjPRZTUr745GhtKCCX"
+
+//#define WIFI_SSID "MGTS_GPON_2829"
+//#define WIFI_PASSWORD "CHTGK9BF"
+
+#define WIFI_SSID "TubondWifi"
+#define WIFI_PASSWORD "qwerty123"
+
+
+// define our sensors
+int lampPos = 0;
+
+WiFiClient client;
+FirebaseData firebaseData;
+
+void setup() {
+  Serial.begin(9600);
+  
+  WiFi.begin (WIFI_SSID, WIFI_PASSWORD);
+
+  //waiting for connection wifi
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(250);
+    Serial.print(".");
+  }
+
+  Serial.println ("");
+  Serial.println ("Connected!");
+  Serial.println(WiFi.localIP());
+
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  
+  Wire.begin(D1, D2); /* задаем i2c мост через контакты SDA=D1 и SCL=D2 на NodeMCU */
+}
+
+void loop() {
+
+  String buff = "";
+
+  Firebase.getInt(firebaseData,"/Led");
+  lampPos = firebaseData.intData();
+  Serial.print("FBout ");
+  Serial.print(lampPos);
+  Serial.println();
+  
+ Wire.beginTransmission(8); /* Начинаем передачу на адресе 8 */
+ Wire.write("{lamp:");  /* Отправляем "hello Arduino" */
+ Wire.write(lampPos);
+ Wire.write("}");
+ Wire.endTransmission();    /* прекращаем передачу */
+
+ delay(50); //10 pls
+ 
+ Wire.requestFrom(8, 26); /* запрашиваем и считываем данные с 8 и 13 адреса slave устройства */
+ while(Wire.available()){
+    char c = Wire.read();
+    buff = buff + c;
+ //   Serial.print(c);
+ }
+
+ Serial.println(buff);
+ Serial.println();
+}
---
+++ I2Cuno.ino
@@ -0,0 +1,51 @@
+#include <Wire.h>
+
+#define LAMP 2
+
+
+int intBuff = 0;
+
+void setup() {
+ lcd.init();
+ 
+ pinMode(LAMP, OUTPUT);
+  
+ Wire.begin(8);                /* задаем на шине i2c 8 адрес */
+ Wire.onReceive(receiveEvent); /* регистрируем полученное событие */
+ Wire.onRequest(requestEvent); /* регистрируем запрошенное событие */
+ Serial.begin(9600);           /* открываем серийный порт для дебаггинга */
+}
+
+void loop() {
+ delay(10);
+}
+
+// Функция для извлечения любых принимаемых данных от мастера на шину
+void receiveEvent(int howMany) {
+ String buffRE = "";
+ while (0 <Wire.available()) {
+    char c = Wire.read();      /* получаем байт как символ*/
+    buffRE = buffRE + c;
+    if(buffRE == "{lamp:"){
+      intBuff = Wire.read();
+      c = Wire.read();
+      buffRE = buffRE + intBuff + c;
+      Serial.println();
+      Serial.println(buffRE); 
+      Serial.println(intBuff); 
+      Serial.println();
+      digitalWrite(LAMP, intBuff);
+      break;
+    }
+   // Serial.print(c);           /* выводим символ в серийный порт */
+  }
+ Serial.println();             /* переходим на новую строку */
+}
+
+// Функция для извлечения любых отправляемых данных от мастера на шину
+void requestEvent() {
+     char buffer__ = intBuff + 48;
+     Wire.write("{lampPosUn:"); 
+     Wire.write(buffer__);
+     Wire.write("}");  
+}
